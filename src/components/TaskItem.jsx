41735827b264bb311e91c12a60f69381dbbb087d
Maryna Zhukovska <marzhukovskaya@gmail.com>
2024-12-03 16:00:28 +0100

    add edit and delete buttons

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,8 +1,18 @@
 function TaskItem({ task, removeTodo }) {
   return (
     <li>
-      {task.text}
-      <button onClick={() => removeTodo(task.id)}>Delete</button>
+      <div>
+        <input id="todo-id" type="checkbox" />
+        <label htmlFor="todo-id">{task.text}</label>
+      </div>
+      <div>
+        <button type="button">
+          Edit <span className="visually-hidden">{task.text}</span>
+       </button>
+        <button type="button" onClick={() => removeTodo(task.id)}>
+          Delete <span className="visually-hidden">{task.text}</span>
+        </button>
+      </div>
     </li>
   );
 }
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -3,7 +3,7 @@ import TaskItem from "./TaskItem";
 function Tasks({todos, removeTodo}) {
     console.log(typeof(todos));
     return (
-        <ul>
+        <ul role="list" aria-labelledby="list-heading">
             {todos.map((todo) => (
                 <TaskItem key={todo.id} task={todo} removeTodo={removeTodo} />
             ))}
